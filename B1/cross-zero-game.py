6e7acca592d1a52d4cedeaa817712d8013884b56
Den DAO$ <667766yden@gmail.com>
2025-10-24 21:35:52 +0300

    B5 m

--- B1/cross-zero-game.py
+++ B1/cross-zero-game.py
@@ -1,8 +1,7 @@
-1
 import random
 
 class Game:
-    def __init__(self, n_dim = 3):
+    def __init__(self, n_dim: int = 3):
         self.n_dim = n_dim
         self.FIELDS_COORD = []
         for i in range(n_dim):
@@ -11,12 +10,9 @@ class Game:
         self.empty_sign = "-"
         self.zero_sign = "0"
         self.cross_sign = "X"
-        self.win_conditions = self.get_win_conditions(n_dim=self.n_dim)
+        self.win_conditions = self._get_win_conditions(n_dim=self.n_dim)
 
-        self.cross_data = []
-        self.zero_data = []
-        # self.field_template = f"    1   2   3 \n1 | - | - | - |\n2 | - | - | - |\n3 | - | - | - |"
-        self.field_template = self.get_empty_field(
+        self.field_template = self._get_empty_field(
             n_dim=self.n_dim,
             empty_sign=self.empty_sign
         )
@@ -31,7 +27,7 @@ class Game:
         self.empty_coords = None
 
     @staticmethod
-    def get_win_conditions(n_dim = 3):
+    def _get_win_conditions(n_dim: int = 3) -> list:
         win_conditions = []
         for i in range(n_dim * n_dim):
             if (i + 1) % n_dim == 0:
@@ -47,7 +43,7 @@ class Game:
         return win_conditions
 
     @staticmethod
-    def get_empty_field(n_dim = 3, empty_sign = "-"):
+    def _get_empty_field(n_dim: int = 3, empty_sign: str = "-") -> str:
         head_str = f"  "
         for i in range(n_dim):
             head_str = f"{head_str}  {i + 1} "
@@ -59,7 +55,7 @@ class Game:
             body_str = f"{body_str}{row_str}\n"
         return body_str
 
-    def input_text(self):
+    def _input_text(self) -> str:
         input_text = input(f"Player {self.current_player} input coordinates. Use format 'row column' with space between\n")
         while True:
             try:
@@ -70,27 +66,26 @@ class Game:
                 input_text = input(f"Player {self.current_player} input coordinates again: \n")
         return input_coord
 
-    def input_step(self):
-        input_coord = self.input_text()
+    def _input_step(self) -> str:
+        input_coord = self._input_text()
         while True:
             if input_coord in self.empty_coords:
                 break
             else:
                 print(f"This coordinates incorrect or has already been used. \n"
                       f"Use one of these free coordinates instead {self.empty_coords}.\n")
-            input_coord = self.input_text()
+            input_coord = self._input_text()
         return input_coord
 
-    def winner_check(self):
+    def _winner_check(self) -> bool:
         if len(self.player_fields_history[f"Player {self.current_player}"]) >= self.n_dim:
             for coord in self.win_conditions:
                 wc = set([id_ for i, id_ in enumerate(self.FIELDS_COORD) if i in coord])
-                # print("wc", wc, coord, set(self.player_fields[f"Player {self.current_player}"]))
                 if len(wc.intersection(set(self.player_fields_history[f"Player {self.current_player}"]))) == self.n_dim:
                     return True
         return False
 
-    def add_step_to_template(self):
+    def _add_step_to_template(self):
         gs = self.indices[self.FIELDS_COORD.index(self.current_coord)]
         self.field_template = self.field_template[:gs] + str(self.current_player) + self.field_template[gs + 1:]
 
@@ -102,46 +97,38 @@ class Game:
             if step == 1:
                 print(f"Game started!!!\n"
                       f"{self.field_template}")
-            print("_" * 50)
+            print("-" * 50)
             print(f"Step {step}")
-            self.current_coord = self.input_step()
+            self.current_coord = self._input_step()
             self.empty_coords.pop(self.empty_coords.index(self.current_coord))
-            self.add_step_to_template()
+            self._add_step_to_template()
             print(self.field_template)
 
             self.history[f"Step {step}"] = (f"Player {self.current_player}", self.current_coord)
             self.player_fields_history[f"Player {self.current_player}"].append(self.current_coord)
 
-            if self.winner_check():
-                print("_" * 50)
+            if self._winner_check():
+                print("-" * 50)
                 print(f"Player {self.current_player} wins!")
-                print("_" * 50)
+                print("-" * 50)
                 break
             self.current_player = self.zero_sign if self.current_player == self.cross_sign else self.cross_sign
             step += 1
 
             if not self.empty_coords:
-                print("_" * 50)
+                print("-" * 50)
                 print(f"Players has a draw!!! Nobody wins, nobody lost!")
-                print("_" * 50)
+                print("-" * 50)
                 break
             if step > self.n_dim ** 2 + 1:
                 break
 
 
 if __name__ == "__main__":
-    game = Game(n_dim=5)
-    # print(game.FIELDS_COORD)
-    print(game.win_conditions)
-    # print(game.field_template)
-    # print(game.indices)
-
-
-    # print(game.get_empty_field(n_dim=5, empty_sign="-"))
+    game = Game(n_dim=3)
     game.start()
-    print(game.history)
-    print(game.player_fields_history)
-    print(game.win_conditions)
+    # print(game.history)
+    # print(game.player_fields_history)
 
 
 
